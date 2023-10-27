7e8b527409be12d55c6f81c507fa88ccccfd33a6
VANESSA ROCHA <vanessa.cardoso@rede.ulbra.br>
2023-10-27 03:30:57 -0300

    .

--- minha-ap2/src/App.js
+++ minha-ap2/src/App.js
@@ -1,14 +1,19 @@
-import logo from './logo.svg';
+
 import './App.css';
 import Gallery from './components/Gallery';
 import Profile from './components/Profile';
+import { Button } from './components/Button';
+import Menu from './components/Menu';
 
 
 function App() {
   return (
     <>
+    <Menu/>
+    <Button/>
     <Profile/>
     <Gallery/>
+  
     </>
   );
 }
---
+++ minha-ap2/src/components/Button.js
@@ -0,0 +1,8 @@
+export const Button = () => {
+    const style = {backgroundColor: "pink", color: "black", borderBottomLeftRadius: 20,
+    borderBottomRightRadius: 20,borderTopLeftRadius: 20,
+    borderTopRightRadius: 20,borderWidth: 0,padding: 20,}
+    return (
+      <button style ={style}>Localização</button>
+    )
+  }
