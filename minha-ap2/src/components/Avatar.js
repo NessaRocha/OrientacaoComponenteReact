0fd7cec603b909f2fb5d1b65d3b25ab6001e2801
VANESSA ROCHA <vanessa.cardoso@rede.ulbra.br>
2023-10-29 19:36:37 -0300

    .

--- minha-ap2/src/components/Avatar.js
+++ minha-ap2/src/components/Avatar.js
@@ -1,13 +1,19 @@
-export default function Avatar({person,size}){
+export default function Avatar(){
+
+    style={{
+        
+    }}
+
+    const avatar = ''; 
+    const description = '';
     
     return (
         <img
         className="avatar"
-        src={(person)}
-        alt={person.name}
-        width={size}
-        height={size}
-        
+        src={avatar}
+        alt={description}
+        width={100}
+        height={100}        
         />
     );
 }
