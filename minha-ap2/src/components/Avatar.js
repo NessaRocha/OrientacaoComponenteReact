f6a922e7724e61839c492c0a60c6de2b863f93e3
VANESSA ROCHA <vanessa.cardoso@rede.ulbra.br>
2023-10-29 19:52:35 -0300

    .

--- minha-ap2/src/components/Avatar.js
+++ minha-ap2/src/components/Avatar.js
@@ -1,19 +1,14 @@
 export default function Avatar(){
-
-    style={{
-        
-    }}
-
-    const avatar = ''; 
-    const description = '';
-    
+    const avatar = 'https://www.sitiodamata.com.br/pub/media/webp_image/wysiwyg/wildflowers-1.webp';
+    const description = 'Aluno';
     return (
         <img
         className="avatar"
         src={avatar}
         alt={description}
-        width={100}
-        height={100}        
+        width={200}
+        height={200}
+        borderRadius={200/2}
         />
     );
 }
--- minha-ap2/src/components/Button.js
+++ minha-ap2/src/components/Button.js
@@ -1,7 +1,6 @@
 export const Button = () => {
-    const style = {backgroundColor: "pink", color: "black", borderBottomLeftRadius: 20,
-    borderBottomRightRadius: 20,borderTopLeftRadius: 20,
-    borderTopRightRadius: 20,borderWidth: 0,padding: 20,}
+    const style = {backgroundColor: "pink", color: "black",
+    borderRadius: 20,borderWidth: 0,padding: 20,}
     return (
       <button style ={style}>Localização</button>
     )
